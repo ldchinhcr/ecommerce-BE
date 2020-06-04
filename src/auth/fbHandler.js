4888a1cb23b57c5e9d9235fc54752ae582ee7cf8
ldchinhcr <58549420+ldchinhcr@users.noreply.github.com>
2020-06-04 15:27:06 +0700

    fix auth handle

--- src/auth/fbHandler.js
+++ src/auth/fbHandler.js
@@ -4,8 +4,8 @@ exports.loginFacebook = passport.authenticate("facebook", { scope: [ 'email' ] }
 
 exports.facebookAuth = function(req, res, next) {
     passport.authenticate("facebook", function(err, user) {
-      if (err) return res.redirect(`https://localhost:3000/`)
-      return res.redirect(`https://localhost:3000/?token=${user.token[user.token.length-1]}&&refreshToken=${user.refreshToken[user.refreshToken.length-1]}`)
+      if (err) return res.redirect(`${process.env.CLIENT_DOMAIN}`)
+      return res.redirect(`${process.env.CLIENT_DOMAIN}/?token=${user.token[user.token.length-1]}&&refreshToken=${user.refreshToken[user.refreshToken.length-1]}`)
     })(req, res, next);
   };
 
--- src/auth/ggHandler.js
+++ src/auth/ggHandler.js
@@ -4,7 +4,7 @@ exports.loginGG = passport.authenticate("google");
 
 exports.ggAuth = function(req, res, next) {
     passport.authenticate("google", function(err, user) {
-      if (err) return res.redirect(`https://localhost:3000/`)
-      return res.redirect(`https://localhost:3000/?token=${user.token[user.token.length-1]}&&refreshToken=${user.refreshToken[user.refreshToken.length-1]}`)
+      if (err) return res.redirect(`${process.env.CLIENT_DOMAIN}`)
+      return res.redirect(`${process.env.CLIENT_DOMAIN}/?token=${user.token[user.token.length-1]}&&refreshToken=${user.refreshToken[user.refreshToken.length-1]}`)
     })(req, res, next);
   };
